7d9fbdfaf1a17064e1096d5d2fdda24dbef1aa36
Your Name <you@example.com>
2022-08-22 21:27:10 +0300

    first commit to server

---
+++ ksafim/src/main/java/com/server/ksafim/com/server/ksafim/KsafimApplication.java
@@ -0,0 +1,13 @@
+package com.server.ksafim.com.server.ksafim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsafimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsafimApplication.class, args);
+	}
+
+}
---
+++ ksafim/src/test/java/com/server/ksafim/KsafimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.ksafim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsafimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
